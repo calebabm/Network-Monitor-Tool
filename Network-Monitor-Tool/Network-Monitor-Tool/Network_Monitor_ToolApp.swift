cbc167050e5bc3dd694f63f19f702b8ef43ac5fe
Caleb Meurer Revelup <caleb.meurer@revelsystems.com>
2021-12-01 08:47:25 -0700

    Initial project creation

---
+++ Network-Monitor-Tool/Network-Monitor-Tool/Network_Monitor_ToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  Network_Monitor_ToolApp.swift
+//  Network-Monitor-Tool
+//
+//  Created by Caleb Meurer on 12/1/21.
+//
+
+import SwiftUI
+
+@main
+struct Network_Monitor_ToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
