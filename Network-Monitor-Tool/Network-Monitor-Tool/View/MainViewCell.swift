ce3f44fa04c7ccf2d3ee1a5a1e074c8c63b18cf9
Caleb Meurer Revelup <caleb.meurer@revelsystems.com>
2021-12-07 18:15:21 -0700

    Animate cell on selection

--- Network-Monitor-Tool/Network-Monitor-Tool/View/MainViewCell.swift
+++ Network-Monitor-Tool/Network-Monitor-Tool/View/MainViewCell.swift
@@ -11,22 +11,51 @@ struct MainViewCell<T: View>: View {
     
     var cellData: CellData
     var destination: T
+    @State var tapped = false
     
     var body: some View {
         ZStack {
-//            Color.offWhite
-            RoundedRectangle(cornerRadius: 25)
-                .fill(Color.offWhite)
-                .frame(width: 300, height: 270)
-                .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
-                .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+            if tapped {
+                RoundedRectangle(cornerRadius: 25)
+                    .fill(Color.offWhite)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 25)
+                            .stroke(Color.gray, lineWidth: 4)
+                                      .blur(radius: 4)
+                                      .offset(x: 2, y: 2)
+                                      .mask(RoundedRectangle(cornerRadius: 25)
+                                                .fill(LinearGradient(Color.black, Color.clear)))
+                    )
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 25)
+                            .stroke(Color.white, lineWidth: 8)
+                                        .blur(radius: 4)
+                                        .offset(x: -2, y: -2)
+                                        .mask(RoundedRectangle(cornerRadius: 25)
+                                                .fill(LinearGradient(Color.clear, Color.black)))
+                    )
+                    .frame(width: 300, height: 270)
+                    .onTapGesture {
+                        tapped.toggle()
+                    }
+            } else {
+                RoundedRectangle(cornerRadius: 25)
+                    .fill(Color.offWhite)
+                    .frame(width: 300, height: 270)
+                    .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
+                    .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+                    .onTapGesture {
+                        tapped.toggle()
+                    }
+            }
             Section {
-                NavigationLink {
+                NavigationLink(isActive: $tapped) {
                     destination
                 } label: {
                     Text(cellData.title)
                         .frame(alignment: .center)
                 }
+
             }
             .frame(width: 300, height: 300, alignment: .center)
         }
@@ -34,19 +63,20 @@ struct MainViewCell<T: View>: View {
     }
 }
 
+
 struct MainViewCell_Previews: PreviewProvider {
     static var previews: some View {
         let cellData = CellData(title: "Local")
         MainViewCell(cellData: cellData, destination: EmptyView())
             .previewLayout(.fixed(width: 300, height: 300))
-
+        
     }
 }
 
 //SwiftUI(iOS 14) currently doesn't allow most customization of NavigationView, so this is a work around
 struct NavigationConfigurator: UIViewControllerRepresentable {
     var configure: (UINavigationController) -> Void = { _ in }
-
+    
     func makeUIViewController(context: UIViewControllerRepresentableContext<NavigationConfigurator>) -> UIViewController {
         UIViewController()
     }
@@ -56,8 +86,15 @@ struct NavigationConfigurator: UIViewControllerRepresentable {
             self.configure(navigationController)
         }
     }
-
+    
 }
+
+extension LinearGradient {
+    init(_ colors: Color...) {
+        self.init(gradient: Gradient(colors: colors), startPoint: .topLeading, endPoint: .bottomTrailing)
+    }
+}
+
 
 extension Color {
     static let offWhite = Color(red: 225 / 255, green: 225 / 255, blue: 235 / 255)
