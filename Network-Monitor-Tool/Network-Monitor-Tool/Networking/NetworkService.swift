2e6922d77439b3d3954cfc41d6dc8afe1a01510d
Caleb Meurer Revelup <caleb.meurer@revelsystems.com>
2022-01-21 10:51:55 -0700

    Clean up network service for imp2 merge

--- Network-Monitor-Tool/Network-Monitor-Tool/Networking/NetworkService.swift
+++ Network-Monitor-Tool/Network-Monitor-Tool/Networking/NetworkService.swift
@@ -8,18 +8,4 @@
 import Foundation
 import NetKitLocal
 
-struct NetworkService {
-    let local = LocalNetworkService()
-    
-    func sendData() {
-        local.send(data: Data(), to: "192.168.86.170") { result in
-            switch result {
-            case .success:
-                print("connection was established")
-            case .failure(let error):
-                fatalError("connection error: \(error)")
-            }
-        }
-    }
-    
-}
+struct NetworkService {}
